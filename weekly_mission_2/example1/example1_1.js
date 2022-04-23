ed00a7946b451c334464f0ca38e514fd271aa182
CrisBarrera <crisbar_02@hotmail.com>
2022-04-23 14:50:26 -0500

    Agregando ejemplo 1.1

---
+++ weekly_mission_2/example1/example1_1.js
@@ -0,0 +1,6 @@
+console.log("Creación de un objeto vacío")
+
+const myFirstObject = {} // empty object
+//console.log(`1. Objeto vacío: ${myFirstObject}`)
+console.log("1. Objeto vacío: ")
+console.log(myFirstObject)
