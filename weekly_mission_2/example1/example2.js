c60b795641e71f77bde00fea23d82c9468f46c43
CrisBarrera <crisbar_02@hotmail.com>
2022-04-23 15:52:40 -0500

    Agregando ejemplo 2

---
+++ weekly_mission_2/example1/example2.js
@@ -0,0 +1,77 @@
+const explorers = [
+    {
+      name: "Explorer 1",
+      exercises_completed: 10,
+      rate: 99,
+      city: "CDMX",
+      stack: [
+        "js",
+        "c#"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: true,
+          exercisesFinished: true
+        },
+        frontend: {
+          isFinished: true,
+          exercisesFinished: true
+        }
+      }
+    },
+    {
+      name: "Explorer 2",
+      exercises_completed: 9,
+      city: "Veracruz",
+      rate: 50,
+      stack: [
+        "js"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: false,
+          exercisesFinished: false
+        },
+        frontend: {
+          isFinished: false,
+          exercisesFinished: false
+        }
+      }
+    },
+    {
+      name: "Explorer 3",
+      exercises_completed: 3,
+      city: "Sonora",
+      rate: 100,
+      stack: [
+        "elixir"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: true,
+          exercisesFinished: true
+        },
+        frontend: {
+          isFinished: false,
+          exercisesFinished: false
+        }
+      }
+    }
+   ]
+
+console.log(explorers)
+
+console.log("Ejercicio 2.1")
+// => es como si se definiera una función 
+explorers.forEach(explorer => console.log(explorer.name))
+console.log("Ejercicio 2.2")
+explorers.forEach(explorer => console.log(explorer.stack))
+
+console.log("Ejercicio 2.3")
+//Map crea una nueva lista apartir de lo que se quiera
+const explorers_stack = explorers.map(explorer => explorer.stack)
+console.log(explorers_stack)
+
+console.log("Ejercicio 2.4")
+const explorers_with_js = explorers.filter(explorer => explorer.stack.includes("js"))
+console.log(explorers_with_js)
