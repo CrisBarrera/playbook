12362d2fb9de968c72c92e1e87c3581a15f446e7
CrisBarrera <crisbar_02@hotmail.com>
2022-04-23 15:31:01 -0500

    Renombrendo nombres

--- weekly_mission_2/example1/example1_1.js
+++
@@ -1,6 +0,0 @@
-console.log("Creación de un objeto vacío")
-
-const myFirstObject = {} // empty object
-//console.log(`1. Objeto vacío: ${myFirstObject}`)
-console.log("1. Objeto vacío: ")
-console.log(myFirstObject)
