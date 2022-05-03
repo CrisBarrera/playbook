817493a9ebe640d572f37ef0a1ad01340c836ed5
CrisBarrera <crisbar_02@hotmail.com>
2022-05-03 16:50:47 -0500

    Agregando constructor a mi clase

--- weekly_mission_2/example2/example2_1.js
+++ weekly_mission_2/example2/example2_1.js
@@ -1,7 +1,7 @@
 console.log("POO")
 
 //Objeto de js
-const woopa = {
+const woopa_obj = {
     name: "Woopa",
     mission: "Node JS",
     age: "12",
@@ -9,8 +9,26 @@ const woopa = {
 }
 
 //Crear una clase
- class Ajolonauta {
+class Ajolonauta {
+    constructor(name, mission, age, color){
+        this.name = name
+        this.mission = mission
+        this.age = age
+        this.color = color
+    }
+}
 
- }
+console.log(Ajolonauta)
 
- console.log(Ajolonauta)
+const woopa = new Ajolonauta("Woopa", "Node JS", 18, "pink") //instanciar un objeto
+const wooper = new Ajolonauta("Wooper", "Java", 10, "Blue")
+//const woopy = new Ajolonauta()
+
+const woopa_obj_js = {}
+
+console.log(woopa)
+console.log(wooper)
+//console.log(woopy)  //Objeto que viene de uan clase
+
+console.log(woopa_obj_js) //Objeto de JS
+
