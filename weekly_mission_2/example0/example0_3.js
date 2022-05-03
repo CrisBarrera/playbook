ec4812958263e29d4c73455744073ca006cb16da
CrisBarrera <crisbar_02@hotmail.com>
2022-05-03 14:48:19 -0500

    Agregando ejemplo para crear clase vacía

---
+++ weekly_mission_2/example0/example0_3.js
@@ -0,0 +1,44 @@
+/// ¿Qué veo?
+/**
+ * Perfil del usuario
+ * -Nombre
+ * -Usuario
+ * -Seguidores
+ * -Seguidos
+ * -Número de Posts
+ * -Bio
+ * Fotos del usuario
+ * -Imagen
+ * -Likes
+ * -Fecha de creación
+ * -Descripción
+ * Mensajes
+ * -Usuario destinatario
+ * -Lista de mensajes
+ * Videos del usuario
+ * -Lista de videos
+ * -Imagen
+ * -Likes
+ * -Fecha de creación
+ * -Descripción
+ * Reels del usuario
+ * -Imagen
+ * -Likes
+ * -Fecha de creación
+ * -Descripción
+ * Fotos etiquetadas
+ * -Imagen
+ * -Likes
+ * -Fecha de creación
+ * -Descripción
+ * -Usuario que etiqueta 
+ */
+
+const ig_user = {
+    name: "User Name",
+    userName: "user name",
+    followers: 100,
+    following: 20,
+    total_posts: 50
+}
+
---
+++ weekly_mission_2/example2/example2_1.js
@@ -0,0 +1,16 @@
+console.log("POO")
+
+//Objeto de js
+const woopa = {
+    name: "Woopa",
+    mission: "Node JS",
+    age: "12",
+    color: "pink"
+}
+
+//Crear una clase
+ class Ajolonauta {
+
+ }
+
+ console.log(Ajolonauta)
